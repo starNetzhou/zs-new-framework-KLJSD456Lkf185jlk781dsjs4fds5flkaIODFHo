fe4fa4bdde493861f9aecfb6145ccc835973f168
周艺成 <morningstarzhou@163.com>
2021-04-15 10:48:35 +0800

    sync sdk

--- bin/zsLibs/framework/zs.ui.js
+++ bin/zsLibs/framework/zs.ui.js
@@ -132,10 +132,6 @@ window.zs.ui = window.zs.ui || {};
         static preload() {
             return Promise((resolve, reject) => { resolve(); });
         }
-        static make() {
-            let view = zs.ui.FGUI_Loading.createInstance();
-            return view;
-        }
         updateProgress(value) {
             if (this.view) {
                 this.view.loadingValue.text = value + '%';
@@ -160,6 +156,7 @@ window.zs.ui = window.zs.ui || {};
             return false
         }
     }
+    Loading.typeDefine = FGUI_Loading;
 
     class LayaLoading extends Laya.Script {
         constructor() {
@@ -196,6 +193,117 @@ window.zs.ui = window.zs.ui || {};
         }
     }
 
+    class uiScene {
+        static get list() {
+            if (this._list == null) {
+                this._list = [];
+            }
+            return this._list;
+        }
+        static init() {
+            this.scene = Laya.stage.addChild(new Laya.Scene3D());
+            this.camera = (this.scene.addChild(new Laya.Camera(0, 0.1, 100)));
+            this.camera.clearFlag = Laya.CameraClearFlags.DepthOnly;
+            this.camera.clearColor = new Laya.Vector4(0, 0, 0, 0);
+            this.camera.transform.position = new Laya.Vector3(0, 0, 3);
+            this.camera.transform.rotation = new Laya.Quaternion();
+            this.camera.enableRender = false;
+            this.light = this.scene.addChild(new Laya.DirectionLight());
+            this.light.color = new Laya.Vector3(1, 1, 1);
+            this.light.transform.worldMatrix.setForward(new Laya.Vector3(1, -1, 0));
+        }
+        static sync(object) {
+            if (object instanceof Laya.Camera) {
+                if (!this.camera) {
+                    return;
+                }
+                this.camera.clearFlag = object.clearFlag;
+                this.camera.clearColor = object.clearColor;
+                this.camera.cullingMask = object.cullingMask;
+                this.camera.aspectRatio = object.aspectRatio;
+                this.camera.nearPlane = object.nearPlane;
+                this.camera.farPlane = object.farPlane;
+                this.camera.fieldOfView = object.fieldOfView;
+                this.camera.orthographic = object.orthographic;
+                this.camera.orthographicVerticalSize = object.orthographicVerticalSize;
+            }
+            else {
+                if (!this.light) {
+                    return;
+                }
+                this.light.transform.rotation = object.transform.rotation;
+                this.light.color = object.color;
+                this.light.intensity = object.intensity;
+            }
+        }
+        static resetCamera() {
+            if (!this.camera) {
+                return;
+            }
+            this.camera.clearFlag = Laya.CameraClearFlags.DepthOnly;
+            this.camera.clearColor = new Laya.Vector4(0, 0, 0, 0);
+            this.camera.aspectRatio = 0;
+            this.camera.nearPlane = 0.1;
+            this.camera.farPlane = 100;
+            this.camera.fieldOfView = 60;
+            this.camera.orthographic = false;
+            this.camera.orthographicVerticalSize = 10;
+            this.camera.addAllLayers();
+            this.camera.transform.position = new Laya.Vector3(0, 0, 3);
+            this.camera.transform.rotation = new Laya.Quaternion();
+        }
+        static resetLight() {
+            if (!this.light) {
+                return;
+            }
+            this.light.transform.worldMatrix.setForward(new Laya.Vector3(1, -1, 0));
+            this.light.color = new Laya.Vector3(1, 1, 1);
+            this.light.intensity = 1;
+        }
+        static add(sprite, position, rotationEuler) {
+            this.scene.addChild(sprite);
+            this.list.push(sprite);
+            sprite.transform.position = position ? position : new Laya.Vector3();
+            sprite.transform.rotationEuler = rotationEuler ? rotationEuler : new Laya.Vector3();
+            this.camera.enableRender = true;
+            return sprite;
+        }
+        static cloneAdd(sprite, position, rotationEuler) {
+            return this.add(sprite.clone(), position, rotationEuler);
+        }
+        static clear() {
+            if (this.list == null || this.list.length <= 0) {
+                return;
+            }
+            for (let i = 0, n = this.list.length; i < n; i++) {
+                this.list[i].removeSelf();
+            }
+            this._list = [];
+            this.camera.enableRender = false;
+        }
+        static removeAt(index) {
+            if (this.list == null || this.list.length <= 0 || index >= this.list.length) { return; }
+            this.list[index].removeSelf();
+            this.list.splice(index, 1);
+            if (this.list.length <= 0) {
+                this.camera.enableRender = false;
+            }
+        }
+        static remove(sprite) {
+            if (this.list == null || this.list.length <= 0) { return; }
+            for (let i = 0, n = this.list.length; i < n; i++) {
+                if (this.list[i].id == sprite.id) {
+                    this.list[i].removeSelf();
+                    this.list.splice(i, 1);
+                    break;
+                }
+            }
+            if (this.list.length <= 0) {
+                this.camera.enableRender = false;
+            }
+        }
+    }
+
     exports.bind = bind;
     exports.readURL = readURL;
     exports.FGUI_item = FGUI_item;
@@ -205,4 +313,5 @@ window.zs.ui = window.zs.ui || {};
     exports.FGUI_Loading = FGUI_Loading;
     exports.Loading = Loading;
     exports.LayaLoading = LayaLoading;
+    exports.uiScene = uiScene;
 }(window.zs.ui = window.zs.ui || {}));
