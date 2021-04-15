c81429b0fe0fffd9cce43432aaca4a64666c9a83
周艺成 <morningstarzhou@163.com>
2021-04-15 10:30:49 +0800

    update index.js

--- bin/index.js
+++ bin/index.js
@@ -12,26 +12,4 @@ loadLib("libs/laya.core.js")
 loadLib("libs/laya.d3.js")
 loadLib("libs/laya.physics3D.js")
 //-----libs-end-------
-if (typeof wx !== "undefined") {
-    loadLib("zsLibs/adapter/tdweapp.js")
-    loadLib("zsLibs/adapter/zs.wx.banner.js")
-    loadLib("zsLibs/adapter/zs.platform.wx.js")
-}
-loadLib("zsLibs/adapter/zs.platform.config.js")
-loadLib("zsLibs/framework/zs.log.js")
-loadLib("zsLibs/framework/zs.resource.js")
-loadLib("zsLibs/framework/zs.utils.js")
-loadLib("zsLibs/framework/zs.td.js")
-loadLib("zsLibs/framework/zs.product.js")
-loadLib("zsLibs/framework/zs.fgui.js")
-loadLib("zsLibs/framework/zs.fsm.js")
-loadLib("zsLibs/framework/zs.platform.js")
-
-loadLib("zsLibs/fairygui/fairygui.js")
-loadLib("zsLibs/framework/zs.ui.js")
-loadLib("zsLibs/framework/zs.exporter.js")
-loadLib("zsLibs/framework/zs.core.js")
-loadLib("zsLibs/framework/zs.network.js")
-loadLib("zsLibs/framework/zs.base.js")
-
-loadLib("js/bundle.js");
+loadLib("zsLibs/index.js")
---
+++ bin/zsLibs/index.js
@@ -0,0 +1,23 @@
+if (typeof wx !== "undefined") {
+    loadLib("zsLibs/adapter/tdweapp.js")
+    loadLib("zsLibs/adapter/zs.wx.banner.js")
+    loadLib("zsLibs/adapter/zs.platform.wx.js")
+}
+loadLib("zsLibs/adapter/zs.platform.config.js")
+loadLib("zsLibs/framework/zs.log.js")
+loadLib("zsLibs/framework/zs.resource.js")
+loadLib("zsLibs/framework/zs.utils.js")
+loadLib("zsLibs/framework/zs.td.js")
+loadLib("zsLibs/framework/zs.product.js")
+loadLib("zsLibs/framework/zs.fgui.js")
+loadLib("zsLibs/framework/zs.fsm.js")
+loadLib("zsLibs/framework/zs.platform.js")
+
+loadLib("zsLibs/fairygui/fairygui.js")
+loadLib("zsLibs/framework/zs.ui.js")
+loadLib("zsLibs/framework/zs.exporter.js")
+loadLib("zsLibs/framework/zs.core.js")
+loadLib("zsLibs/framework/zs.network.js")
+loadLib("zsLibs/framework/zs.base.js")
+
+loadLib("js/bundle.js");
