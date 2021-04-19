e5d00c296ac69b6c45834d1f8a91c01fbefeec51
周艺成 <morningstarzhou@163.com>
2021-04-19 17:47:50 +0800

    fix zs.core.js bug

--- bin/zsLibs/framework/zs.core.js
+++ bin/zsLibs/framework/zs.core.js
@@ -502,7 +502,7 @@ window.zs = window.zs || {};
             }
 
             this.checkGameCfg(switchs);
-            this.checkPanelSort();
+            Laya.timer.frameLoop(1, null, () => { this.checkPanelSort(); });
 
             if (this.onPrepare) {
                 this.onPrepare.run();
@@ -624,18 +624,16 @@ window.zs = window.zs || {};
             }
         }
         static checkPanelSort() {
-            Laya.timer.frameLoop(1, null, () => {
-                let sortIndex = 1
-                if (zs.ui.uiScene.scene) {
-                    if (Laya.stage.getChildIndex(zs.ui.uiScene.scene) < Laya.stage.numChildren - sortIndex) {
-                        Laya.stage.setChildIndex(zs.ui.uiScene.scene, Laya.stage.numChildren - sortIndex);
-                    }
-                    sortIndex++;
+            let sortIndex = 1
+            if (zs.ui.uiScene.scene) {
+                if (Laya.stage.getChildIndex(zs.ui.uiScene.scene) < Laya.stage.numChildren - sortIndex) {
+                    Laya.stage.setChildIndex(zs.ui.uiScene.scene, Laya.stage.numChildren - sortIndex);
                 }
-                if (Laya.stage.getChildIndex(fairygui.GRoot.inst.displayObject) != Laya.stage.numChildren - sortIndex) {
-                    Laya.stage.setChildIndex(fairygui.GRoot.inst.displayObject, Laya.stage.numChildren - sortIndex);
-                }
-            });
+                sortIndex++;
+            }
+            if (Laya.stage.getChildIndex(fairygui.GRoot.inst.displayObject) != Laya.stage.numChildren - sortIndex) {
+                Laya.stage.setChildIndex(fairygui.GRoot.inst.displayObject, Laya.stage.numChildren - sortIndex);
+            }
         }
         static checkGameCfg(switchs) {
             let gamecfg = zs.configs.gameCfg;
