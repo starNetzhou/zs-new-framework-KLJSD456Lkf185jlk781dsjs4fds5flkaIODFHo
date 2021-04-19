558ee0dac36b5afc8a27e54cb4130a35abed0905
周艺成 <morningstarzhou@163.com>
2021-04-19 10:47:15 +0800

    prepare sdk example

--- src/script/GameLogic.ts
+++ src/script/GameLogic.ts
@@ -5,6 +5,7 @@ export default class GameLogic extends Laya.Script {
     exampleSceneURL: string = "example/ExampleUI.scene";
 
     exampleUI: ExampleUI;
+    curScene: Laya.Scene3D;
 
     constructor() {
         super();
@@ -13,7 +14,7 @@ export default class GameLogic extends Laya.Script {
 
     init() {
         Laya.Scene3D.load("Conventional/TestScene.ls", Laya.Handler.create(this, (s) => {
-            Laya.stage.addChild(s);
+            this.curScene = Laya.stage.addChild(s) as Laya.Scene3D;
             Laya.Scene.open(this.exampleSceneURL, true, null, Laya.Handler.create(this, (s) => {
                 let scene = s as Laya.Scene;
                 scene.height = Laya.stage.height;
@@ -22,6 +23,10 @@ export default class GameLogic extends Laya.Script {
                     this.exampleUI.setWorkflowState("btn clicked")
                     console.log("Test Example Btn Click");
                 })
+            }));
+            Laya.Sprite3D.load("Conventional/Ball.lh", Laya.Handler.create(this, (s) => {
+                let sprite = this.curScene.addChild(s) as Laya.Sprite3D;
+                sprite.transform.position = new Laya.Vector3(0, 3, 0);
             }));
         }))
     }
